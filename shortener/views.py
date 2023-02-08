b0145d95f0581416d6d7292eb444d41fc5d76f57
김태균(KIM Teagyun)/Data Intelligence팀/SK <10347@skcc.com>
2023-02-08 13:37:50 +0900

    Django Debug Toolbar

---
+++ shortener/views.py
@@ -0,0 +1,27 @@
+from django.http.response import JsonResponse
+from shortener.models import Users
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+
+def index(request):
+    user = Users.objects.filter(username="admin").first()
+    email = user.email if user else "Anonymous User!"
+    return render(request, "base.html", {"welcome_msg": f"Hello {email}", "hello": "world"})
+
+
+@csrf_exempt
+def get_user(request, user_id):
+    print(user_id)
+    if request.method == "GET":
+        abc = request.GET.get("abc")
+        xyz = request.GET.get("xyz")
+        user = Users.objects.filter(pk=user_id).first()
+        return render(request, "base.html", {"user": user, "params": [abc, xyz]})
+    elif request.method == "POST":
+        username = request.GET.get("username")
+        if username:
+            user = Users.objects.filter(pk=user_id).update(username=username)
+        return JsonResponse(status=201, data=dict(msg="You just reached with Post Method!"), safe=False)
