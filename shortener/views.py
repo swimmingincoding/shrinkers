338dcf3f4e5fab6cb04e5e1b8a0ff7d417692c5f
swimmingincoding <gngkgl3@naver.com>
2023-02-10 17:17:24 +0900

    Django 게시판 1

--- shortener/views.py
+++ shortener/views.py
@@ -12,13 +12,20 @@ from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    user = Users.objects.filter(username="admin").first()
+    user = Users.objects.filter(id=request.user.id).first()
     email = user.email if user else "Anonymous User!"
     print("Logged in?", request.user.is_authenticated)
     if request.user.is_authenticated is False:
         email = "Anonymous User!"
     print(email)
     return render(request, "base.html", {"welcome_msg": f"Hello {email}"})
+
+
+def url_list(request):
+    return render(
+        request,
+        "url_list.html",
+    )
 
 
 @csrf_exempt
