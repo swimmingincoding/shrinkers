55b5c0fffa676b9769d48c27c0c04bc0674ad3e1
김태균(KIM Teagyun)/Data Intelligence팀/SK <10347@skcc.com>
2023-02-22 14:29:20 +0900

    Django DRF Swagger 구현

--- shortener/urls/serializers.py
+++ shortener/urls/serializers.py
@@ -44,7 +44,6 @@ class UrlCreateSerializer(serializers.Serializer):
                 print(e)
             else:
                 url_count_changer(request, True)
-        print(instance)
         return instance
 
 
